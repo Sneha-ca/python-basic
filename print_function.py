49c4298ed2085584399b009981716ca1e23465a4
Sneha Kumar <snehaqwertyu214@gmail.com>
2026-01-30 22:57:11 +0530

    Update print_function.py

--- print_function.py
+++ print_function.py
@@ -1,1 +1,27 @@
+#1.
+print("Hello i am sneha.")
+
+#2.
+a = (14,31,'sneha')
+print(a)
+
+#3.
+name="sneha"
+age = 19
+print("NAME:",name)
+print("AGE:",age)
+
+#3.
+name="sneha"
+age=19
+print("Hello my name is",name,"and my age is",age,"years old.")
+
+#4.
+print('Online python compiler is a wonderful'+'website.')
+
+#5.
+a=27
+b=01
+c=2026
+print(a,b,c, sep="-")
 
